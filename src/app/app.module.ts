1e44f743844696d2bbf3f46cac26f770d3b163b5
Mariia Kuryleva <maru.kuryleva@gmail.com>
2023-08-21 12:48:54 +0200

    feat: add pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { FavoritesPageComponent } from './pages/favorites-page/favorites-page.component';
+import { PhotoPageComponent } from './pages/photo-page/photo-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, NotFoundComponent, MainPageComponent, FavoritesPageComponent, PhotoPageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
