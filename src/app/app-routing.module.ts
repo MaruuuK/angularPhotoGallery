32d8d641459c9e39384caaa65f2c452b6bfa8230
Mariia Kuryleva <maru.kuryleva@gmail.com>
2023-08-21 14:43:40 +0200

    feat: styled active element

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+//import { PhotoPageComponent } from './pages/photo-page/photo-page.component';
+import { FavoritesPageComponent } from './pages/favorites-page/favorites-page.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'favorites',
+    component: FavoritesPageComponent,
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+  },
+  { path: '**', redirectTo: '/not-found' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
